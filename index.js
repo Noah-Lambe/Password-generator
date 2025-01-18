01a2f8a44cc1c93a14f5a51e125c8230566f7910
Noah Lambe <noahalambe@gmail.com>
2025-01-18 00:00:51 -0330

    completed password generation with a default length of 8, user may choose a desired length

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const process = require("process");
+
 const arguments = process.argv.slice(2);
+const userInput = arguments;
 
-let password;
+let password = "";
 
-for (let i = 0; i < arguments.length; i++) {
-  if (arguments[i] === "--generate") {
-    password = arguments[i + 1];
+const generatePassword = (userInput) => {
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  const charactersLength = characters.length;
+  let passwordLength = 8;
+
+  if (userInput.includes("--length")) {
+    passwordLength = arguments[1];
+    for (let i = 0; i < passwordLength; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * charactersLength)
+      );
+    }
+  } else {
+    for (let i = 0; i < passwordLength; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * charactersLength)
+      );
+    }
   }
-}
+
+  console.log("Generated Password:", password);
+};
+
+generatePassword(userInput);
